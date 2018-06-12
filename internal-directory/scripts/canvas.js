59b775b463c7dab5cbde33b33d8e3de86b3c176a
Aram Hammoudeh <aramcontact@gmail.com>
2018-06-11 22:48:21 -0600

    Added map generator and player movement. Too tired to work any more. Need sleep.

--- internal-directory/scripts/canvas.js
+++ internal-directory/scripts/canvas.js
@@ -1,48 +1,137 @@
 'use strict';
+// Initialize 
+var ctx = null;
 
-
-let canvas = document.querySelector('#canvas');
-let context = canvas.getContext('2d');
-
-let grass = new Image();
-let water = new Image();
-let dirt = new Image();
-
-grass.src = './assets/grass1.png';
-water.src = './assets/snow1.png';
-dirt.src = './assets/rock1.png';
-
-
-
-let xPos = 0;
-let yPos = 0;
-
-let map = [
-  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 2, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 2, 2, 0, 0, 0, 0],
-  [0, 0, 0, 1, 2, 2, 0, 0, 0, 0],
-  [0, 0, 0, 1, 2, 2, 0, 0, 0, 0]
+//Game Map
+var gameMap = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 1, 1, 0, 1, 1, 1, 1, 0,
+  0, 1, 0, 0, 0, 1, 0, 0, 0, 0,
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+  0, 1, 0, 1, 0, 0, 0, 1, 1, 0,
+  0, 1, 0, 1, 0, 1, 0, 0, 1, 0,
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+  0, 1, 0, 0, 0, 0, 0, 1, 0, 0,
+  0, 1, 1, 1, 0, 1, 1, 1, 1, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ];
+// Configs
+var tileW = 30, tileH = 15;
+var mapW = 10, mapH = 10;
+var currentSecond = 0, frameCount = 0, framesLastSecond = 0;
+var lastFrameTime = 0;
 
-for (let i = 0; i < map.length; i++) {
-  for (let y = 0; y < map[i].length; y++) {
-    if (map[i][y] === 0) {
-      context.drawImage(grass, xPos, yPos);
-    }
-    if (map[i][y] === 1) {
-      context.drawImage(dirt, xPos, yPos);
-    }
-    if (map[i][y] === 2) {
-      context.drawImage(water, xPos, yPos);
-    }
-    // xPos += 50;
+// Key set to false
+var keysDown = {
+  37: false,
+  38: false,
+  39: false,
+  40: false
+};
+
+var player = new Character();
+
+function Character() {
+  this.tileFrom = [1, 1];
+  this.tileTo = [1, 1];
+  this.timeMoved = 0;
+  this.dimensions = [25, 10];
+  this.position = [45, 45];
+  this.delayMove = 700;
+}
+Character.prototype.placeAt = function (x, y) {
+  this.tileFrom = [x, y];
+  this.tileTo = [x, y];
+  this.position = [((tileW * x) + ((tileW - this.dimensions[0]) / 2)),
+    ((tileH * y) + ((tileH - this.dimensions[1]) / 2))];
+};
+Character.prototype.processMovement = function (t) {
+  if (this.tileFrom[0] == this.tileTo[0] && this.tileFrom[1] == this.tileTo[1]) { return false; }
+
+  if ((t - this.timeMoved) >= this.delayMove) {
+    this.placeAt(this.tileTo[0], this.tileTo[1]);
   }
-  // yPos += 50;
+  else {
+    this.position[0] = (this.tileFrom[0] * tileW) + ((tileW - this.dimensions[0]) / 2);
+    this.position[1] = (this.tileFrom[1] * tileH) + ((tileH - this.dimensions[1]) / 2);
+
+    if (this.tileTo[0] != this.tileFrom[0]) {
+      var diff = (tileW / this.delayMove) * (t - this.timeMoved);
+      this.position[0] += (this.tileTo[0] < this.tileFrom[0] ? 0 - diff : diff);
+    }
+    if (this.tileTo[1] != this.tileFrom[1]) {
+      var diff = (tileH / this.delayMove) * (t - this.timeMoved);
+      this.position[1] += (this.tileTo[1] < this.tileFrom[1] ? 0 - diff : diff);
+    }
+
+    this.position[0] = Math.round(this.position[0]);
+    this.position[1] = Math.round(this.position[1]);
+  }
+
+  return true;
+};
+
+function toIndex(x, y) {
+  return ((y * mapW) + x);
 }
 
+window.onload = function () {
+  ctx = document.getElementById('canvas').getContext('2d');
+  requestAnimationFrame(drawGame);
+  ctx.font = 'bold 10pt sans-serif';
 
+  window.addEventListener('keydown', function (e) {
+    if (e.keyCode >= 37 && e.keyCode <= 40) { keysDown[e.keyCode] = true; }
+  });
+  window.addEventListener('keyup', function (e) {
+    if (e.keyCode >= 37 && e.keyCode <= 40) { keysDown[e.keyCode] = false; }
+  });
+};
 
+function drawGame() {
+  if (ctx == null) { return; }
+
+  var currentFrameTime = Date.now();
+  var timeElapsed = currentFrameTime - lastFrameTime;
+
+  var sec = Math.floor(Date.now() / 1000);
+  if (sec != currentSecond) {
+    currentSecond = sec;
+    framesLastSecond = frameCount;
+    frameCount = 1;
+  }
+  else { frameCount++; }
+
+  if (!player.processMovement(currentFrameTime)) {
+    if (keysDown[38] && player.tileFrom[1] > 0 && gameMap[toIndex(player.tileFrom[0], player.tileFrom[1] - 1)] == 1) { player.tileTo[1] -= 1; }
+    else if (keysDown[40] && player.tileFrom[1] < (mapH - 1) && gameMap[toIndex(player.tileFrom[0], player.tileFrom[1] + 1)] == 1) { player.tileTo[1] += 1; }
+    else if (keysDown[37] && player.tileFrom[0] > 0 && gameMap[toIndex(player.tileFrom[0] - 1, player.tileFrom[1])] == 1) { player.tileTo[0] -= 1; }
+    else if (keysDown[39] && player.tileFrom[0] < (mapW - 1) && gameMap[toIndex(player.tileFrom[0] + 1, player.tileFrom[1])] == 1) { player.tileTo[0] += 1; }
+
+    if (player.tileFrom[0] != player.tileTo[0] || player.tileFrom[1] != player.tileTo[1]) { player.timeMoved = currentFrameTime; }
+  }
+
+  for (var y = 0; y < mapH; ++y) {
+    for (var x = 0; x < mapW; ++x) {
+      switch (gameMap[((y * mapW) + x)]) {
+      case 0:
+        ctx.fillStyle = '#685b48';
+        break;
+      default:
+        ctx.fillStyle = '#5aa457';
+      }
+
+      ctx.fillRect(x * tileW, y * tileH, tileW, tileH);
+    }
+  }
+
+  ctx.fillStyle = '#0000ff';
+  ctx.fillRect(player.position[0], player.position[1],
+    player.dimensions[0], player.dimensions[1]);
+
+  ctx.fillStyle = '#ff0000';
+  ctx.fillText('FPS: ' + framesLastSecond, 10, 20);
+
+  lastFrameTime = currentFrameTime;
+  requestAnimationFrame(drawGame);
+}
